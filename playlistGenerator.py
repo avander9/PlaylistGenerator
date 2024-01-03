b3f371eb5ae94dc5c80535375e1ec2c46e508209
Alexander <6461978+avander9@users.noreply.github.com>
2024-01-03 09:54:31 +0100

    initial commit

---
+++ playlistGenerator.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+
+def generate_playlist_m3u(directory):
+    files = []
+
+    try:
+        files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
+    except FileNotFoundError:
+        print(f"The directory '{directory}' was not found.")
+        return
+    except PermissionError:
+        print(f"You don't have permissions to access the directory '{directory}'.")
+        return
+
+    video_files = [file for file in files if file.lower().endswith((".mp4", ".mkv", ".avi", ".mov"))]
+
+    playlist_path = os.path.join(directory, 'playlist.m3u')
+    with open(playlist_path, 'w') as playlist_file:
+        for file in video_files:
+            playlist_file.write(file + '\n')
+
+    print(f"Playlist generated successfully: {playlist_path}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python generate_playlist.py /path/to/directory")
+    else:
+        target_directory = sys.argv[1]
+        generate_playlist_m3u(target_directory)
